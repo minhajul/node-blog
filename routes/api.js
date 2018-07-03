609def6743897c75c34a59911f72927000037938
Md.Minhajul Islam <kmsohelinfo@gmail.com>
2018-07-04 00:03:59 +0600

    #1: Protected blogs route

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 const express = require('express'),
       router = express.Router(),
-      apiController = require('../controllers/apiController');
+      apiController = require('../controllers/apiController'),
+      expressJWT = require('express-jwt');
 
-router.get('/blogs', apiController.blogs);
+const JwtChecker = expressJWT({
+    secret: "my-secret-keys"
+});
+
+router.get('/blogs', JwtChecker, apiController.blogs);
 
 module.exports = router;
