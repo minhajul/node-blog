0b7d91fe7fa8cd90fe4d6329e8a7ba86b74c9839
Md.Minhajul Islam <kmsohelinfo@gmail.com>
2018-07-09 22:40:49 +0600

    Removed unnecessary code

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,6 @@ const blogs = require('../data/blogs.json');
 
 exports.home = async (req, res) => {
     res.render('index', {
-        title: 'Express App',
         blogs
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ const express = require('express'),
     homeController = require('../controllers/homeController'),
     blogController = require('../controllers/blogController');
 
-const data = require('../data/blogs');
-
 router.get('/', homeController.home);
 
 router.get('/blog', blogController.index);
