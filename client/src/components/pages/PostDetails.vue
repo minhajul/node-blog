6ae86907aa52a6a72371e015e48d45263dc3e945
Md.Minhajul Islam <kmsohelinfo@gmail.com>
2018-08-29 00:58:33 +0600

    #10: Remove unnecessary css

--- client/src/components/pages/PostDetails.vue
+++ client/src/components/pages/PostDetails.vue
@@ -32,23 +32,3 @@
     }
   }
 </script>
-
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-</style>
