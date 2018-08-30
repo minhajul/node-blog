59dc0bbfe8cfdb7f27fdd1bff743e54cf694d6d8
Md.Minhajul Islam <kmsohelinfo@gmail.com>
2018-08-31 02:35:39 +0600

    Display logged in users email after login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,17 @@
 <template>
+  <div class="">
+
+    <NavBar/>
+
     <router-view/>
+  </div>
 </template>
 
 <script>
+  import NavBar from "./components/NavBar";
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {NavBar}
   }
 </script>
